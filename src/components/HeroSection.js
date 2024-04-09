a350166d0082abd616599f771029a0562c829fc5
jainyashmessi <messiyash2312@gmail.com>
2024-04-09 18:38:44 +0530

    First react Project

---
+++ src/components/HeroSection.js
@@ -0,0 +1,37 @@
+function HeroSection(){
+    return(
+        <div>
+            <main className="hero container">
+                <div className="Hero-content">
+                    <h1>YOUR FEET DESERVE THE BEST</h1>
+
+                    <p>YOU FEET DERSEVE THE BEST WE ARE HERE TO HELP YOU WIHT OUR SHOES.
+                    YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU OTHER SHOES </p>
+                  <div className="Hero-button">
+
+                    <button>Shope Now</button>
+                    <button className="secondary-btn">Catagory</button>
+
+                </div>
+                <div className="shoping">
+                    <p>Also Available On</p>
+                     <div className="Filip-AmazonImg">
+                     <img src="Amazon.logo.png" alt="err"/>
+                     <img src="Filipkar.png"alt="err" className="second-img"/>
+                     </div>
+                    
+                </div>
+                    
+                </div>
+                <div className="Hero-imge">
+                    <img src="Nike shoes.png" alt="error"></img>
+
+                </div>
+
+            </main>
+        </div>
+    )
+
+
+}
+export default HeroSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+
+
+function Navbar(){
+    return(
+        <div>
+            <nav className="container">
+                <div className="logo">
+                  <img src="nike logo image.png" alt="error"></img>
+                </div>
+                
+                    <ul>
+                        <li href="#">Menu</li>
+                        <li href="#">Location</li>
+                        <li href="#">About</li>
+                        <li href="#">Contact</li>
+                    </ul>
+                    <button>Login</button>
+
+                
+            </nav>
+        </div>
+    )
+}
+export default Navbar;
